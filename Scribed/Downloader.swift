c2387530e28c4f073b47ae7c240d1d711f612c67
Chtun <chtun@live.com>
2024-11-16 19:45:27 -0600

    Changed Name

---
+++ Scribed/Downloader.swift
@@ -0,0 +1,8 @@
+//
+//  Downloader.swift
+//  Scribed
+//
+//  Created by Pooja Kalikiri on 11/16/24.
+//
+
+import Foundation
---
+++ Scribed/ThumbnailGenerator.swift
@@ -0,0 +1,8 @@
+//
+//  ThumbnailGenerator.swift
+//  Scribed
+//
+//  Created by Pooja Kalikiri on 11/16/24.
+//
+
+import Foundation
